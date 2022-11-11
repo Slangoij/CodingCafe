7f7be0f549bd97a282edfb48c82203cad7dec75e
ij_slango <junginje@gmail.com>
2022-11-12 08:47:17 +0900

    파일명 변경 및 마커 추가

---
+++ script.js
@@ -0,0 +1,34 @@
+// script.js
+var map;
+
+function initMap() {
+  var seoul = { lat: 37.5642135 ,lng: 127.0016985 };
+  map = new google.maps.Map( document.getElementById('map'), {
+      zoom: 12,
+      center: seoul
+    });
+
+  new google.maps.Marker({
+    position: seoul,
+    map: map,
+    label: "서울 중심 좌표"
+  });
+  
+  var cityHall = {lat: 37.566554, lng: 126.978546};
+
+  var defaultOptions = {
+      zoom: 12,
+      center: cityHall,
+      disableDefaultUI:true,
+      zoomControl: true
+      };
+
+  map = new google.maps.Map(document.getElementById('map'), defaultOptions);
+
+  var myIcon = new google.maps.MarkerImage("./src/marker_cafecoding_resized.png");
+  var marker = new google.maps.Marker({
+      position: cityHall,
+      map: map,
+      icon: myIcon,
+  });
+}
